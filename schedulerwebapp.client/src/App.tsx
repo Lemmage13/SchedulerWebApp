ff38fdf560c0cad00e8dab1457a23781ffa64adf
Matt <matthewwilliam.cole@gmail.com>
2024-09-05 14:22:07 +0100

    added token context and login function

--- schedulerwebapp.client/src/App.tsx
+++ schedulerwebapp.client/src/App.tsx
@@ -3,18 +3,26 @@ import WeekPlanner from './WeekPlanner'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import ProtectedRoutes from "./Tools/ProtectedRoutes"
 import Register from './Register';
+import { createContext, useState } from 'react';
+import ITokenContext from './interfaces/TokenContext';
+
+export const TokenContext = createContext<ITokenContext>({})
 function App() {
+    const [token, setToken] = useState(null)
+    const tokenContextValue: ITokenContext = {token, setToken}
 
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route element={<Login />} path="/login" />
-                <Route element={<Register/> } path="/register" />
-                <Route element={<ProtectedRoutes />}>
-                    <Route element={<WeekPlanner />} path="/planner" />
-                </Route>
+        <TokenContext.Provider value={tokenContextValue}>
+            <BrowserRouter> 
+                <Routes>
+                    <Route element={<Login />} path="/login" />
+                    <Route element={<Register/> } path="/register" />
+                    <Route element={<ProtectedRoutes />}>
+                        <Route element={<WeekPlanner />} path="/planner" />
+                    </Route>
             </Routes>
-        </BrowserRouter>
+            </BrowserRouter>
+        </TokenContext.Provider>
     );
 }
 
--- schedulerwebapp.client/src/Login.tsx
+++ schedulerwebapp.client/src/Login.tsx
@@ -1,6 +1,33 @@
 import { Box, Button, Card, CardActions, CardHeader, Stack, TextField } from "@mui/material"
+import { SetStateAction, useContext } from "react"
+import { TokenContext } from "./App"
+import axios from "axios"
+import { useState } from "react"
 
 function Login() {
+    const tokenContext = useContext(TokenContext)
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    async function handleLogin() {
+        try {
+            const response = await axios.post("https://localhost:54249/api/Auth/login",
+                {
+                    email: email,
+                    password: password
+                })
+            console.log(response.data)
+            tokenContext.setToken? tokenContext.setToken(response.data.token) : console.error("SET TOKEN UNDEFINED")
+        }
+        catch (e) {
+            console.error(e)
+        }
+    }
+    function handleEmailChange(e: { target: { value: SetStateAction<string> } }): void {
+        setEmail(e.target.value)
+    }
+    function handlePasswordChange(e: { target: { value: SetStateAction<string> } }): void {
+        setPassword(e.target.value)
+    }
     return (
         <Box
             sx={[{ display: "flex" },
@@ -13,12 +40,13 @@ function Login() {
                     spacing={2}>
                     <CardHeader
                         title="Log in" />
-                    <TextField size="small" id="userName" label="User Name" />
-                    <TextField size="small" type="password" id="password" label="Password" />
+                    <TextField size="small" id="email" label="Email" value={email} onChange={handleEmailChange} />
+                    <TextField size="small" type="password" id="password" label="Password" value={password} onChange={handlePasswordChange} />
                     <CardActions sx={{ justifyContent: "center" }}>
                         <Button
                             sx={{ width: 1 }}
-                            size="small">Submit</Button>
+                            size="small"
+                            onClick={handleLogin }>Submit</Button>
                     </CardActions>
                 </Stack>
             </Card>
