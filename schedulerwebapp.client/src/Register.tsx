187f40e6b2e9667b38c0f3f5d47eb9ca4b250e30
Matt <matthewwilliam.cole@gmail.com>
2024-09-10 16:21:11 +0100

    Added functionality to register page

--- schedulerwebapp.client/src/Register.tsx
+++ schedulerwebapp.client/src/Register.tsx
@@ -1,6 +1,59 @@
-import { Button, Card, CardActions, CardHeader, Container, Stack, TextField } from "@mui/material"
+import { Button, Card, CardActions, CardHeader, Container, Stack, TextField, Typography } from "@mui/material"
+import { SetStateAction, useContext } from "react"
+import { TokenContext } from "./App"
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function Register() {
+    const tokenContext = useContext(TokenContext);
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [pw1, setPw1] = useState("");
+    const [pw2, setPw2] = useState("");
+    const [pwSame, setPwSame] = useState(true)
+
+    const nav = useNavigate()
+
+    async function handleRegister():Promise<void> {
+        try {
+            const response = await axios.post("https://localhost:54249/api/Auth/register",
+                {
+                    name: username,
+                    email: email,
+                    password: pw1
+                })
+            console.log(response.data)
+            tokenContext.setToken ? tokenContext.setToken(response.data.token) : console.error("SET TOKEN UNDEFINED")
+            nav("/planner")
+        }
+        catch (e) {
+            console.error(e)
+        }
+    }
+
+
+    function handleUsernameChange(e: { target: { value: SetStateAction<string> } }): void {
+        setUsername(e.target.value)
+    }
+    function handleEmailChange(e: { target: { value: SetStateAction<string> } }): void {
+        setEmail(e.target.value)
+    }
+    function handlePw1Change(e: { target: { value: SetStateAction<string> } }): void {
+        setPw1(e.target.value)
+    }
+    function handlePw2Change(e: { target: { value: SetStateAction<string> } }): void {
+        setPw2(e.target.value)
+    }
+
+    useEffect(() => {
+        if (pw1 == pw2) {
+            setPwSame(true)
+        }
+        else {setPwSame(false) }
+    }, [pw1, pw2])
+
     return (
         <Container
             sx={[{ display: "flex" },
@@ -13,13 +66,17 @@ function Register() {
                     spacing={2}>
                     <CardHeader
                         title="Register" />
-                    <TextField size="small" id="userName" label="User Name" />
-                    <TextField size="small" type="password" id="password" label="Password" />
-                    <TextField size="small" type="password" id="passwordConfirmation" label="Confirm Password"/>
+                    <TextField size="small" id="userName" label="User Name" onChange={handleUsernameChange} />
+                    <TextField size="small" id="email" label="Email" onChange={handleEmailChange} />
+                    <TextField size="small" type="password" id="password" label="Password" onChange={handlePw1Change} />
+                    <TextField size="small" type="password" id="passwordConfirmation" label="Confirm Password" onChange={handlePw2Change} />
+                    {!pwSame && <Typography>Passwords must be identical</Typography>}
                     <CardActions sx={{justifyContent: "center"} }>
                         <Button
-                            sx={{width: 1} }
-                            size="small">Submit</Button>
+                            sx={{ width: 1 }}
+                            size="small"
+                            disabled={!pwSame}
+                            onClick={handleRegister}>Submit</Button>
                     </CardActions>
                 </Stack>
             </Card>
